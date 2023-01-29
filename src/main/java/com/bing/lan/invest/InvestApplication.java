68ee9138f7a7201ff94bd2e9faf13c5b94aa84da
lanboys <lan_bing2013@163.com>
2023-01-29 14:29:08 +0800

    配置mybatis-plus

---
+++ src/main/java/com/bing/lan/invest/InvestApplication.java
@@ -0,0 +1,36 @@
+package com.bing.lan.invest;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.util.List;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication
+@MapperScan("com.bing.lan.invest.mapper")
+public class InvestApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext application = SpringApplication.run(InvestApplication.class, args);
+
+        Environment env = application.getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "应用 '{}' 运行成功! \n" +
+                        "----------------------------------------------------------",
+                env.getProperty("spring.application.name"));
+
+        synchronized (application) {
+            try {
+                application.wait();
+            } catch (InterruptedException e) {
+                log.error("应用运行结束...", e);
+            }
+        }
+    }
+
+}
